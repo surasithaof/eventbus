525d2197b76bed459b2b767cba29608e8643c583
Surasith <surasith.kae@ascendcorp.com>
2025-04-15 20:54:57 +0700

    test: update benchmark test

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
 [![license](https://img.shields.io/github/license/surasithaof/eventbus)](https://github.com/surasithaof/eventbus/blob/master/LICENSE.md)
 
 A simple Go package for implementing an internal async event bus. This is for learning Goroutine to handle asynchronous event publishing and subscription, making it efficient and easy to integrate with Go apps.
+
+## Benchmark
+
+```sh
+go test -benchtime 10000000x -benchmem -run=^$ -bench=.
+```
+
+```
+goos: darwin
+goarch: arm64
+pkg: github.com/surasithaof/eventbus
+cpu: Apple M1
+BenchmarkSubscribe-8    10000000                53.07 ns/op           42 B/op          0 allocs/op
+BenchmarkPublish-8      10000000               346.5 ns/op            96 B/op          2 allocs/op
+PASS
+ok      github.com/surasithaof/eventbus 4.221s
+```
--- benchmark_test.go
+++ benchmark_test.go
@@ -2,23 +2,32 @@ package eventbus_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/surasithaof/eventbus"
 )
 
-func BenchmarkEventBus(b *testing.B) {
+func BenchmarkSubscribe(b *testing.B) {
+	b.ReportAllocs()
+
+	eventbus := eventbus.NewEventBus[string]()
+	defer eventbus.StopAndWait()
+
+	for i := 0; i < b.N; i++ {
+		eventbus.Subscribe(TestEventType, func(ctx context.Context, data string) {})
+	}
+}
+
+func BenchmarkPublish(b *testing.B) {
+	b.ReportAllocs()
+
 	eventbus := eventbus.NewEventBus[string]()
 	defer eventbus.StopAndWait()
 	ctx := context.Background()
 
-	eventbus.Subscribe(TestEventType, func(ctx context.Context, data string) {
-		b.Logf("Received data: %s", data)
-		b.Logf("Context value: %v", ctx.Value(testCtxKey))
-	})
+	eventbus.Subscribe(TestEventType, func(ctx context.Context, data string) {})
+
 	for i := 0; i < b.N; i++ {
-		ctx = context.WithValue(ctx, testCtxKey, i)
-		eventbus.Publish(ctx, TestEventType, fmt.Sprintf("test message %d", i))
+		eventbus.Publish(ctx, TestEventType, "test message")
 	}
 }
